e1d44671580ccfe34a620ba5f156e03a4d6d8b60
ekimovde <61349359+ekimovde@users.noreply.github.com>
2021-03-27 20:18:56 +0700

    Refactoring Disk

--- src/utils/disk.js
+++ src/utils/disk.js
@@ -1,10 +1,28 @@
 let alphStr = "abcdefghijklmnopqrstuvwxyz";
 
 const disk = {
+  setCheckKey(text, key) {
+    if (key.length < text.length) {
+      while (key.length < text.length) {
+        key = key + key;
+      }
+    }
+
+    if (key.length > text.length) {
+      let newKey = key;
+      const countSymbol = key.length - text.length;
+
+      key = newKey.substring(0, newKey.length - countSymbol);
+    }
+
+    return key;
+  },
   encoding: (text, keyValueAlph, keyValueText, lang) => {
     let newAlph = "";
-    let str = "";
+    let strKey = "";
     let array = [];
+    let encodedStr = "";
+
     text = text.replace(/\s/g, "");
 
     if (lang === "Русский") {
@@ -23,28 +41,42 @@ const disk = {
       }
     }
 
-    console.log("newAlph:", newAlph);
+    console.log("newAlph", newAlph);
 
-    for (let i = 0; i < keyValueText.length; i++) {
-      let index = alphStr.indexOf(keyValueText[i]);
-      array.push(index);
+    strKey = disk.setCheckKey(text, keyValueText);
+
+    console.log("strKey:", strKey);
+
+    for (let i = 0; i < alphStr.length; i++) {
+      let positionStart = newAlph.length - i;
+      let subStr = newAlph.slice(positionStart);
+      let string = subStr + newAlph.replace(subStr, "");
+
+      array.push(string);
     }
+
+    console.log("array:", array);
 
     for (let i = 0; i < text.length; i++) {
-      let index = alphStr.indexOf(text[i]);
-      let indexArr = array[i % array.length];
-      let charIndex = index - indexArr;
+      let indexText = alphStr.indexOf(text[i]);
+      let indexKey = alphStr.indexOf(strKey[i]);
+      let tmp = array[indexKey];
 
-      charIndex = charIndex < 0 ? charIndex + newAlph.length : charIndex;
-      str += newAlph[charIndex % newAlph.length];
+      encodedStr += tmp[indexText];
     }
 
-    return str;
+    console.log("encodedStr:", encodedStr);
+    console.log("\n");
+
+    return encodedStr;
   },
+
   decoding: (text, keyValueAlph, keyValueText, lang) => {
     let newAlph = "";
-    let str = "";
+    let strKey = "";
     let array = [];
+    let decodedStr = "";
+
     text = text.replace(/\s/g, "");
 
     if (lang === "Русский") {
@@ -63,22 +95,33 @@ const disk = {
       }
     }
 
-    console.log("newAlph:", newAlph);
+    console.log("newAlph", newAlph);
 
-    for (let i = 0; i < keyValueText.length; i++) {
-      let index = alphStr.indexOf(keyValueText[i]);
-      array.push(index);
+    strKey = disk.setCheckKey(text, keyValueText);
+
+    console.log("strKey:", strKey);
+
+    for (let i = 0; i < alphStr.length; i++) {
+      let positionStart = newAlph.length - i;
+      let subStr = newAlph.slice(positionStart);
+      let string = subStr + newAlph.replace(subStr, "");
+
+      array.push(string);
     }
+
+    console.log("array:", array);
 
     for (let i = 0; i < text.length; i++) {
-      let index = newAlph.indexOf(text[i]);
-      let indexArr = array[i % array.length];
-      let charIndex = index + indexArr;
+      let indexKey = alphStr.indexOf(strKey[i]);
+      let tmp = array[indexKey];
+      let indexText = tmp.indexOf(text[i]);
 
-      str += alphStr[charIndex % alphStr.length];
+      decodedStr += alphStr[indexText];
     }
 
-    return str;
+    console.log("decodedStr:", decodedStr);
+
+    return decodedStr;
   },
 };
 
