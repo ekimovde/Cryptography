8e99547504e9424786e74b40fbac2ea32ebeb89d
ekimovde <ekimov-denis@bauart.pro>
2021-04-11 20:02:42 +0700

    Fix code vigener

--- src/utils/vigener.js
+++ src/utils/vigener.js
@@ -1,11 +1,10 @@
+const arr_ru = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+const arr_en = "abcdefghijklmnopqrstuvwxyz";
+const arr_RU = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+const arr_EN = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 const vigener = {
-  encoding: (text, key, type) => {
-    const arr_ru = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-    const arr_en = "abcdefghijklmnopqrstuvwxyz";
-
-    const arr_RU = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-    const arr_EN = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+  encoding: (text, key) => {
     let strKey = key;
     let strEncoded = "";
 
@@ -32,94 +31,118 @@ const vigener = {
         strEncoded += text[i];
       } else {
         if (arr_en.indexOf(text[i]) !== -1) {
-          let indexText = arr_en.indexOf(text[i]);
-          let indexKey = arr_en.indexOf(strKey[i]);
+          let indexText = parseInt(arr_en.indexOf(text[i]));
+          let indexKey = parseInt(arr_en.indexOf(strKey[i]));
 
-          if (arr_EN.indexOf(strKey[i]) !== -1) {
-            indexKey = arr_EN.indexOf(strKey[i]);
-          }
+          let index = (indexText + indexKey) % arr_en.length;
 
-          if (type === "Расшифровать") {
-            if (indexText - indexKey < 0) {
-              strEncoded += arr_en[indexText + 26 - indexKey];
-            } else {
-              strEncoded += arr_en[indexText - indexKey];
-            }
-          } else {
-            if (indexText + indexKey < 26) {
-              strEncoded += arr_en[indexText + indexKey];
-            } else {
-              strEncoded += arr_en[indexText + indexKey - 26];
-            }
-          }
+          strEncoded += arr_en[index];
         } else if (arr_EN.indexOf(text[i]) !== -1) {
-          let indexText = arr_EN.indexOf(text[i]);
-          let indexKey = arr_EN.indexOf(strKey[i]);
+          let indexText = parseInt(arr_EN.indexOf(text[i]));
+          let indexKey = parseInt(arr_EN.indexOf(strKey[i]));
 
-          if (arr_en.indexOf(strKey[i]) !== -1) {
-            indexKey = arr_en.indexOf(strKey[i]);
-          }
+          let index = (indexText + indexKey) % arr_EN.length;
 
-          if (type === "Расшифровать") {
-            if (indexText - indexKey < 0) {
-              strEncoded += arr_EN[indexText + 26 - indexKey];
-            } else {
-              strEncoded += arr_EN[indexText - indexKey];
-            }
-          } else {
-            if (indexText + indexKey < 26) {
-              strEncoded += arr_EN[indexText + indexKey];
-            } else {
-              strEncoded += arr_EN[indexText + indexKey - 26];
-            }
-          }
+          strEncoded += arr_EN[index];
         } else if (arr_ru.indexOf(text[i]) !== -1) {
-          let indexText = arr_ru.indexOf(text[i]);
-          let indexKey = arr_ru.indexOf(strKey[i]);
+          let indexText = parseInt(arr_ru.indexOf(text[i]));
+          let indexKey = parseInt(arr_ru.indexOf(strKey[i]));
 
-          if (arr_RU.indexOf(strKey[i]) !== -1) {
-            indexKey = arr_RU.indexOf(strKey[i]);
-          }
+          let index = (indexText + indexKey) % arr_ru.length;
 
-          if (type === "Расшифровать") {
-            if (indexText - indexKey < 0) {
-              strEncoded += arr_ru[indexText + 26 - indexKey];
-            } else {
-              strEncoded += arr_ru[indexText - indexKey];
-            }
-          } else {
-            if (indexText + indexKey < 26) {
-              strEncoded += arr_ru[indexText + indexKey];
-            } else {
-              strEncoded += arr_ru[indexText + indexKey - 26];
-            }
-          }
+          strEncoded += arr_ru[index];
         } else if (arr_RU.indexOf(text[i]) !== -1) {
-          let indexText = arr_RU.indexOf(text[i]);
-          let indexKey = arr_RU.indexOf(strKey[i]);
+          let indexText = parseInt(arr_RU.indexOf(text[i]));
+          let indexKey = parseInt(arr_RU.indexOf(strKey[i]));
 
-          if (arr_ru.indexOf(strKey[i]) !== -1) {
-            indexKey = arr_ru.indexOf(strKey[i]);
-          }
+          let index = (indexText + indexKey) % arr_RU.length;
 
-          if (type === "Расшифровать") {
-            if (indexText - indexKey < 0) {
-              strEncoded += arr_RU[indexText + 26 - indexKey];
-            } else {
-              strEncoded += arr_RU[indexText - indexKey];
-            }
-          } else {
-            if (indexText + indexKey < 26) {
-              strEncoded += arr_RU[indexText + indexKey];
-            } else {
-              strEncoded += arr_RU[indexText + indexKey - 26];
-            }
-          }
+          strEncoded += arr_RU[index];
         }
       }
     }
 
     return strEncoded;
+  },
+  decoding: (text, key) => {
+    let strKey = key;
+    let strDecoded = "";
+
+    if (text.length > strKey.length) {
+      while (strKey.length !== text.length) {
+        for (let i = 0; i < key.length; i++) {
+          if (text.length !== strKey.length) {
+            strKey += key[i];
+          } else {
+            break;
+          }
+        }
+      }
+    }
+
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] === " ") {
+        strDecoded += " ";
+      } else if (text[i] === ",") {
+        strDecoded += ",";
+      } else if (text[i] === ".") {
+        strDecoded += ".";
+      } else if (/^\d+$/.test(+text[i])) {
+        strDecoded += text[i];
+      } else {
+        if (arr_en.indexOf(text[i]) !== -1) {
+          let indexText = parseInt(arr_en.indexOf(text[i]));
+          let indexKey = parseInt(arr_en.indexOf(strKey[i]));
+
+          let index = (indexText - indexKey) % arr_en.length;
+
+          if (index > 0) {
+            strDecoded += arr_en[index];
+          } else {
+            index = arr_en.length - Math.abs(index);
+            strDecoded += arr_en[index];
+          }
+        } else if (arr_EN.indexOf(text[i]) !== -1) {
+          let indexText = parseInt(arr_EN.indexOf(text[i]));
+          let indexKey = parseInt(arr_EN.indexOf(strKey[i]));
+
+          let index = (indexText - indexKey) % arr_EN.length;
+
+          if (index > 0) {
+            strDecoded += arr_EN[index];
+          } else {
+            index = arr_EN.length - Math.abs(index);
+            strDecoded += arr_EN[index];
+          }
+        } else if (arr_ru.indexOf(text[i]) !== -1) {
+          let indexText = parseInt(arr_ru.indexOf(text[i]));
+          let indexKey = parseInt(arr_ru.indexOf(strKey[i]));
+
+          let index = (indexText - indexKey) % arr_ru.length;
+
+          if (index > 0) {
+            strDecoded += arr_ru[index];
+          } else {
+            index = arr_ru.length - Math.abs(index);
+            strDecoded += arr_ru[index];
+          }
+        } else if (arr_RU.indexOf(text[i]) !== -1) {
+          let indexText = parseInt(arr_RU.indexOf(text[i]));
+          let indexKey = parseInt(arr_RU.indexOf(strKey[i]));
+
+          let index = (indexText - indexKey) % arr_RU.length;
+
+          if (index > 0) {
+            strDecoded += arr_RU[index];
+          } else {
+            index = arr_RU.length - Math.abs(index);
+            strDecoded += arr_RU[index];
+          }
+        }
+      }
+    }
+
+    return strDecoded;
   },
 };
 
